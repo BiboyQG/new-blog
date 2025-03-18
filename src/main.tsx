c68e787367a315cbf9c158fa87d9f3a19a0020d6
Banghao Chi <banghao2@illinois.edu>
2025-03-18 06:04:43 -0500

    delete: db files

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import initApp from "./utils/initApp";
-
-// Initialize the application (database, etc.)
-initApp().catch(console.error);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);

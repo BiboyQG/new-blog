9b39a36e7af5cd1e384481b437ff436be57d8c72
Banghao Chi <banghao2@illinois.edu>
2025-03-18 06:00:36 -0500

    feat: LinkedIn

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -56,7 +56,7 @@ export default function Welcome() {
           </svg>
         </a>
         <a
-          href="https://www.youtube.com/channel/UCKIv0_DuIRtKXZLvQZ8inyA"
+          href="https://www.youtube.com/@biboyog"
           target="_blank"
           rel="noopener noreferrer"
           className="text-gray-700 dark:text-gray-300 hover:text-gray-900 dark:hover:text-white"
@@ -78,7 +78,7 @@ export default function Welcome() {
           </svg>
         </a>
         <a
-          href="/rss"
+          href="https://www.linkedin.com/in/banghao-chi-550737276/"
           target="_blank"
           rel="noopener noreferrer"
           className="text-gray-700 dark:text-gray-300 hover:text-gray-900 dark:hover:text-white"
@@ -93,13 +93,14 @@ export default function Welcome() {
             strokeWidth="2"
             strokeLinecap="round"
             strokeLinejoin="round"
-            className="feather feather-rss"
+            className="feather feather-linkedin"
           >
-            <path d="M4 11a9 9 0 0 1 9 9"></path>
-            <path d="M4 4a16 16 0 0 1 16 16"></path>
-            <circle cx="5" cy="19" r="1"></circle>
+            <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
+            <rect x="2" y="9" width="4" height="12"></rect>
+            <circle cx="4" cy="4" r="2"></circle>
           </svg>
         </a>
+        
       </div>
     </div>
   );

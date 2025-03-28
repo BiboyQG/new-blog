2e8552c66d6a021c2ac0587b9e032454ee8b1763
Banghao Chi <banghao2@illinois.edu>
2025-03-28 11:31:20 -0500

    feat: syntax highlight for code block

--- src/components/MarkdownDisplay.tsx
+++ src/components/MarkdownDisplay.tsx
@@ -2,6 +2,7 @@ import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import rehypeRaw from "rehype-raw";
 import rehypeSanitize from "rehype-sanitize";
+import rehypeHighlight from "rehype-highlight";
 
 interface MarkdownDisplayProps {
   content: string;
@@ -16,7 +17,7 @@ export default function MarkdownDisplay({
     <div className={`prose dark:prose-invert max-w-none ${className}`}>
       <ReactMarkdown
         remarkPlugins={[remarkGfm]}
-        rehypePlugins={[rehypeRaw, rehypeSanitize]}
+        rehypePlugins={[rehypeRaw, rehypeSanitize, rehypeHighlight]}
       >
         {content}
       </ReactMarkdown>
